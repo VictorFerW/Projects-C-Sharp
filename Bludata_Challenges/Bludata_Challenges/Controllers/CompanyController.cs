e315f38e78e0741c086c8c71dc09364d5e6e0b91
VictorFerW <victorwarmling10@gmail.com>
2023-08-09 20:28:21 -0300

    Desafio da Bludata

---
+++ Bludata_Challenges/Bludata_Challenges/Controllers/CompanyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bludata_Challenges.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterCompany()
+        {
+            return View();
+        }
+
+        public IActionResult EditCompany()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteCompany()
+        {
+            return View();
+        }
+
+    }
+}
